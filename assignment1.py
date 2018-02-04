508565237d560289ff9bf44aee0e8b1336ae690f
nyzairbud <alasgarcia27@gmail.com>
2018-02-03 22:36:04 -0500

    second try

--- assignment1.py
+++ assignment1.py
@@ -1,13 +1,17 @@
 class ListDivideException(Exception):
     pass
-def listDivide(numbers = ([]), divide = 2):
+
+
+def listDivide(numbers=([]), divide=2):
     count = 0
     for n in numbers:
         if n % divide == 0:
             count = count + 1
         else:
             raise ListDivideException
-        return count
+    return count
+
 
 def testlistDivide(self):
-    listDivide([1,2,3,4,5])
+    if __name__ == '__main__':
+        testlistDivide()
