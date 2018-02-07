dcf6556e0eae6bf16eceb1e2121d0b73646bf61b
nyzairbud <alasgarcia27@gmail.com>
2018-02-06 20:42:24 -0500

    the right one

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -5,14 +5,29 @@ class ListDivideException(Exception):
 def listDivide(numbers=([]), divide=2):
     count = 0
     for n in numbers:
-        if n % divide == 0:
+        if (int(n)% int(divide)) == 0:
             count = count + 1
-        else:
-            raise ListDivideException
     return count
 
 
-def testlistDivide(listDivide):
-    if __name__ == '__main__':
-        testlistDivide()
-
+def testListDivide():
+    if listDivide([1, 2, 3, 4, 5]) == 2:
+        print "the answer is 2"
+    else:
+        raise ListDivideException
+    if listDivide([2, 4, 6, 8, 10]) == 5:
+        print "the answer is 5"
+    else:
+        raise ListDivideException
+    if listDivide([30, 54, 63,98, 100], divide=10) == 2:
+        print "the answer is 2"
+    else:
+        raise ListDivideException
+    if listDivide([]) == 0:
+        print "the answer is 0"
+    else:
+        raise ListDivideException
+    if listDivide([1,2,3,4,5], 1) == 5:
+        print "the answer is 5"
+    else:
+        raise ListDivideException
